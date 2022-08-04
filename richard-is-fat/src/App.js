8fb7fc18eea192c4b932ad4c1d93c3e5ab982e67
Terrence Shi <terrenceshi@gmail.com>
2022-08-04 16:18:56 -0400

    jkhg

---
+++ richard-is-fat/src/App.js
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant = "h1">fuck richard</Typography>
+    </div>
+  );
+}
+
+export default App;
